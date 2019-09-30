61ba4a96709c7ff9bfeae20a93c278453599b94e
dee del rosario <dee@adadevelopersacademy.org>
2019-09-30 11:00:50 -0700

    creates bookscontroller and index action

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+BOOKS = [
+  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+  { title: "Kindred", author: "Octavia E. Butler"}
+]
+
+class BooksController < ApplicationController
+
+  def index
+    @books = BOOKS
+  end
+
+end
