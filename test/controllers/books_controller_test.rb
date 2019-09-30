65029680a930451d4234f33a79945ec80172c81f
dee del rosario <dee@adadevelopersacademy.org>
2019-09-30 14:48:54 -0700

    adds test for the index action

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 describe BooksController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  
+  describe "index action" do
+
+    it "gives back a successful response" do
+
+      # Arrange
+      # ... Nothing right now!
+
+      # Act
+      # Send a specific request... a GET request to the path "/books"
+      get "/books"
+      # get books_path
+
+      # Assert
+      # The response was OK!
+      must_respond_with :success
+      # must_respond_with 200
+      # must_respond_with :redirect
+      # must_respond_with :missing
+      # must_respond_with :error
+
+    end
+
+    it "gives back a 404 if there are no books available" do
+
+      # Arrange
+      # Insert some code here that destroys all of the books in the database...
+
+      # Act
+      get "/books"
+
+      # Assert
+      # must_respond_with :missing
+    end
+
+  end
+
 end
