d9b1791e5b826f0e524b19ae8798829041fc0aa1
dee del rosario <dee@adadevelopersacademy.org>
2019-09-30 12:16:31 -0700

    adds the view for the index action for the books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,11 @@ class BooksController < ApplicationController
 
   def index
     @books = BOOKS
+    # @name = {first_name: "Dee"}
+
+    # By default, our controllers will look for an appropriate view (aka something in views/books/index.html.erb)
+    # If I WANTED to, I can tell the controller to give back a certain format-type and status code (like below:)
+    # return head :not_found
   end
 
 end
