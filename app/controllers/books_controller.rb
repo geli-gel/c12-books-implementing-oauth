4d2a586f5c0cc538bf33fa102eef499b44ef5e98
dee del rosario <dee@adadevelopersacademy.org>
2019-10-01 12:40:12 -0700

    defines root_path to books#index, adds notes about controller behavior

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,13 @@ class BooksController < ApplicationController
     # By default, our controllers will look for an appropriate view (aka something in views/books/index.html.erb)
     # If I WANTED to, I can tell the controller to give back a certain format-type and status code (like below:)
     # return head :not_found
+
+    # By default, our controllers will look for a view to send back, along with assuming it is a status code of 200
+    # If we need to specify otherwise, we will do so in the controller
   end
 
   def show
+    # If we need to access something from the route... (like a route param), we will access it from the params hash-like object
     book_id = params[:id].to_i
     @book = BOOKS[book_id]
     if @book.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # When I want to redefine the value of the variable root_path (so it goes to a specific controller and action), I use this syntax:
+  root :to => "books#index"
+
   get '/books', to: 'books#index'
 
   get '/books/:id', to: 'books#show'
