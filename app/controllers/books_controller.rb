01e91cb90e8290de4465b4edc923b7040fe444f2
dee del rosario <dee@adadevelopersacademy.org>
2019-10-02 14:57:54 -0700

    finishes the new form and create action for new books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,22 @@ class BooksController < ApplicationController
   end
 
   def new
+    # This new "empty" instance of the Book model is used in the view's form... When it's "empty", the form will be empty
     @book = Book.new
+  end
+
+  def create
+    # Step 2: Create an action so that the form data actually gets processed by Rails (by the server) and creates that new book, and changes the database... Also, if there is an error in changing the database, we will handle it here.
+
+    # First, we will access the form data from the new book form using params and its very specific data structure
+
+    @book = Book.new( title: params[:book][:title], author: params[:book][:author], description: params[:book][:description] )
+
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render new_book_path
+    end
   end
 
 end
