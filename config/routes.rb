3be4e4dd7c33fe7d02ff189c0ba1361f3cfe948f
Jared Maddox <jared@adadevelopersacademy.org>
2019-10-02 13:36:24 -0700

    Add 7 restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # When I want to redefine the value of the variable root_path (so it goes to a specific controller and action), I use this syntax:
   root :to => "books#index"
 
-  get '/books', to: 'books#index'
+  # Routes that operate on the book collection
+  get '/books', to: 'books#index', as: 'books'
+  get '/books/new', to: 'books#new', as: 'new_book'
+  post '/books', to: 'books#create'
 
-  get '/books/:id', to: 'books#show'
-
+  # Routes that operate on individual books
+  get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
 end
