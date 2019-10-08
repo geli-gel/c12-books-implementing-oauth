b405d3be19d67e6ebc23974ff9db78f4ce4500f3
dee del rosario <dee@adadevelopersacademy.org>
2019-10-08 11:30:24 -0700

    adding validations and error handling on the book new form to book title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :author
 
+  validates :title, presence: true
+
   def self.all_in_alpha_order
     return Book.all.order(title: :asc)
   end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe Book do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "validations" do
+
+    # Dee's tip: Make a variable in a before or let block that is always a valid book
+    
+    it "can be valid" do
+      # Arrange
+      # Arranging all of the attributes of a new book so that they are valid
+
+      # Act
+      # Making a book, and then triggering validation check
+
+      # Assert
+      # our book.valid? IS true
+      # maybe we will test that there are no errors
+    end
+
+    it "is invalid if there is no title" do
+      # Arrange
+      # Get the valid book, then INVALIDATE IT by re-assigning the title with an invalid value
+      book.title = ""
+
+      # Act
+      # Trigger the validation check
+
+      # Assert
+      # our book.valid? is false
+      # possibly check the structure of .errors
+
+
+    end
+
+
+  end
+
 end
