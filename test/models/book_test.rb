9f233b94004b1423bf411676fb9439a3b374fd7d
dee del rosario <dee@adadevelopersacademy.org>
2019-10-14 11:07:02 -0700

    adds tests for relationships between books and author for book

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -24,6 +24,57 @@ describe Book do
       refute( is_valid )
     end
 
+    # Pseudocode for presence validation test ... IF IT HAD ONE
+    # it is invalid if the title is not unique
+
+      # Arrange
+      # we need an author to make at least one valid book...
+      # We need to make a book!
+      # We need to save this first book...
+      # WE NEED TO MAKE ANOTHER BOOK! ... which has the same title as the first book
+
+      # Act
+      # trigger a validation check
+
+      # Assert
+      # it is not valid
+
+  end
+
+  describe "relationships" do
+
+    it 'can set the author through "author"' do
+      # Arrange
+      # There is an author
+      # There is a book
+      author = Author.create!(name: "test author")
+      book = Book.new(title: "test book")
+
+      # Act
+      # Set the book's author using .author
+      book.author = author
+
+      # Assert
+      # The book's author is the author we're expecting
+      expect( book.author_id ).must_equal author.id
+    end
+
+    it 'can set the author through "author_id"' do
+      # Arrange
+      # There is an author
+      # There is a book
+      author = Author.create!(name: "test author")
+      book = Book.new(title: "test book")
+
+      # Act
+      # Set the book's author using .author
+      book.author_id = author.id
+
+      # Assert
+      # The book's author is the author we're expecting
+      expect( book.author ).must_equal author
+    end
+
   end
 
 end
